1991ee459671389717607b5e23ddc9c108563027
Maarten Bonsema <Zalastra@users.noreply.github.com>
2014-02-08 16:01:26 +0100

    restructured project folder layout for build tool

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -1,20 +1,40 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      build: {
-        src: ['../js/Board.js', '../js/CandyBlock.js', '../js/Blocks.js', '../js/Line.js', '../js/candyrain.js'],
-        dest: 'output/candyrain.min.js'
-      }
-    }
-  });
-
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		useminPrepare: {
+			html: '../src/index.html',
+			options: {
+				dest: 'output'
+			}
+		},
+		usemin: {
+			html: ['output/index.html']
+		},
+		copy: {
+			main: {
+				files: [
+					{src: '../src/index.html', dest: 'output/index.html'},
+					{expand: true, cwd: '../src/images/', src: '*', dest: 'output/images/'},
+					{expand: true, cwd: '../src/js/lib/', src: '*', dest: 'output/js/lib/'}
+				]
+			}
+		}
+	});
+	
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-usemin');
+	
+	grunt.registerTask('default', [
+		'copy:main',
+		'useminPrepare',
+		'concat',
+		'cssmin',
+		'uglify',
+		'usemin'
+		]);
 };
